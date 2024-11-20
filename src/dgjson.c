e1307fd17dc552d31b4faba205027f09c81232c4
David Sheinberg <David_Sheinberg@brown.edu>
2024-11-20 17:47:39 -0500

    added element functions

--- src/dgjson.c
+++ src/dgjson.c
@@ -86,6 +86,61 @@ json_t *dl_to_json(DYN_LIST *dl)
   return array;
 }
 
+json_t *dl_element_to_json(DYN_LIST *dl, int element_id)
+{
+  if (element_id >= DYN_LIST_N(dl)) return NULL;
+
+  if (DYN_LIST_DATATYPE(dl) == DF_LIST) {
+    json_t *subarray = NULL;
+    
+    DYN_LIST **vals = (DYN_LIST **) DYN_LIST_VALS(dl);
+    DYN_LIST *curlist;
+
+    curlist = (DYN_LIST *) vals[element_id];
+    if (curlist) {
+      subarray = dl_to_json(curlist);
+    }
+    return subarray;
+  }
+
+  else {
+    switch (DYN_LIST_DATATYPE(dl)) {
+    case DF_LONG:
+      {
+	int *vals = (int *) DYN_LIST_VALS(dl);
+	return json_integer(vals[element_id]);
+      }
+      break;
+    case DF_SHORT:
+      {
+	short *vals = (short *) DYN_LIST_VALS(dl);
+	return json_integer(vals[element_id]);
+      }
+      break;      
+    case DF_CHAR:
+      {
+	char *vals = (char *) DYN_LIST_VALS(dl);
+	return json_integer(vals[element_id]);
+      }
+      break;      
+    case DF_FLOAT:
+      {
+	float *vals = (float *) DYN_LIST_VALS(dl);
+	return json_real(vals[element_id]);
+      }
+      break;      
+    case DF_STRING:
+      {
+	char **vals = (char **) DYN_LIST_VALS(dl);
+	return json_string(vals[element_id]);
+      }
+      break;
+    default:
+      return NULL;
+    }
+  }
+}
+
 
 json_t *dg_to_json(DYN_GROUP *dg)
 {
@@ -106,3 +161,23 @@ json_t *dg_to_json(DYN_GROUP *dg)
 }
 
 
+json_t *dg_element_to_json(DYN_GROUP *dg, int row)
+{
+  int i;
+  json_t *json_dg, *json_list;
+  DYN_LIST *dl;
+  
+  json_dg = json_object();
+  
+  for (i = 0; i < DYN_GROUP_NLISTS(dg); i++) {
+    dl = DYN_GROUP_LIST(dg, i);
+    if (row < DYN_LIST_N(dl)) {
+      json_t *obj = dl_element_to_json(dl, row);
+      if (obj) {
+	json_object_set_new(json_dg, DYN_LIST_NAME(DYN_GROUP_LIST(dg, i)), obj);
+      }
+    }
+  }
+  
+  return json_dg;
+}
