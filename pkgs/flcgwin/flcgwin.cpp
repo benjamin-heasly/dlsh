3bd8d48ad47bc6bb29fccb777038fcdc0e8808fa
David Sheinberg <David_Sheinberg@brown.edu>
2025-04-04 14:34:11 -0400

    added tab manager support

--- pkgs/flcgwin/flcgwin.cpp
+++ pkgs/flcgwin/flcgwin.cpp
@@ -5,8 +5,7 @@
 
 #include <tcl.h>
 #include "flcgwin.hpp"
-
-#include <unordered_map>
+#include "cg_tab_manager.h"
 
 static void tab_close_cb(Fl_Widget* o, void* data)
 {
@@ -26,6 +25,8 @@ static void tab_close_cb(Fl_Widget* o, void* data)
 	  break;
 	}
       }
+      // Remove from tab manager
+      CGTabManager::getInstance().removeCGWin(o->label());
       Fl::delete_widget(o);
     }
     break;
@@ -35,7 +36,7 @@ static void tab_close_cb(Fl_Widget* o, void* data)
 }
 
 static int
-add_cgraph_func(ClientData data, Tcl_Interp *interp,
+add_cgraph_tab_func(ClientData data, Tcl_Interp *interp,
 		int objc, Tcl_Obj *const objv[])
 {
   if (objc != 2) {
@@ -46,9 +47,12 @@ add_cgraph_func(ClientData data, Tcl_Interp *interp,
   Fl_Tabs *tabs = (Fl_Tabs *) data;
   tabs->begin();
   
+  // Get next tab name from manager
+  std::string tab_name = CGTabManager::getInstance().getNextTabName();
+  
   auto cgwin_widget = new CGWin(interp, tabs->x(), tabs->y()+25,
 				tabs->w(), tabs->h()-25,
-				strdup(Tcl_GetString(objv[1])));
+				tab_name.c_str());
   tabs->value(cgwin_widget);
   cgwin_widget->box(FL_NO_BOX);
   cgwin_widget->selection_color(FL_BACKGROUND_COLOR);
@@ -61,6 +65,9 @@ add_cgraph_func(ClientData data, Tcl_Interp *interp,
   cgwin_widget->when(FL_WHEN_CLOSED);
   cgwin_widget->redraw();
   tabs->end();
+  
+  // Add to tab manager
+  CGTabManager::getInstance().addCGWin(tab_name, cgwin_widget);
   
   return TCL_OK;
 }
@@ -82,6 +89,30 @@ static void tab_callback(Fl_Widget* widget, void* data) {
     }
 }
 
+static int
+select_cgraph_tab_func(ClientData data, Tcl_Interp *interp,
+		int objc, Tcl_Obj *const objv[])
+{
+  if (objc != 2) {
+    Tcl_WrongNumArgs(interp, 1, objv, "name");
+    return (TCL_ERROR);
+  }
+
+  const char* tab_name = Tcl_GetString(objv[1]);
+  CGWin* widget = CGTabManager::getInstance().getCGWin(tab_name);
+  if (widget) {
+    Fl_Tabs* tabs = (Fl_Tabs*)data;
+    tabs->value(widget);
+    currentCG = widget;
+    gbSetGeventBuffer(currentCG->gb());
+    widget->redraw();
+    return TCL_OK;
+  }
+
+  Tcl_SetResult(interp, "Tab not found", TCL_STATIC);
+  return TCL_ERROR;
+}
+
 // Extension initialization function
 extern "C" int Flcgwin_Init(Tcl_Interp *interp)
 {
@@ -99,8 +130,10 @@ extern "C" int Flcgwin_Init(Tcl_Interp *interp)
   tabs->callback(tab_callback);
   
   Tcl_CreateObjCommand(interp,
-		       "add_cgraph", (Tcl_ObjCmdProc *) add_cgraph_func, 
+		       "cgAddTab", (Tcl_ObjCmdProc *) add_cgraph_tab_func, 
 		       (ClientData) tabs, NULL);
-  
+  Tcl_CreateObjCommand(interp,
+		       "cgSelectTab", (Tcl_ObjCmdProc *) select_cgraph_tab_func, 
+		       (ClientData) tabs, NULL);
   return TCL_OK;
 }
