50f3679e28f09c55f226747f94f9323acf454e0e
Ben Heasly <benjamin.heasly@gmail.com>
2025-07-08 15:01:51 -0400

    Add mdns package to dlsh.zip.

--- pkgs/mdns/pkgIndex.tcl
+++ pkgs/mdns/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded mdns 1.0 [list load [file join $dir tcl_mdns[info sharedlibextension]]]
+package ifneeded mdns 1.0 [list load [file join $dir $::tcl_platform(os) $::tcl_platform(machine) tcl_mdns[info sharedlibextension]]]
